0a3280ac7257a2828734d2209da48096f4ab39e1
cauefurui <cauefurui@gmail.com>
2024-04-11 16:59:21 -0300

    feat: creating sidebar

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,25 @@
+import { LogOut } from 'lucide-react'
+import Image from 'next/image'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <Image
+        src="https://github.com/cauefurui.png"
+        alt="Perfil"
+        width={40}
+        height={40}
+        className="rounded-full"
+      />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">Caue Furui</span>
+        <span className="truncate text-sm text-zinc-500">
+          cauefurui@gmail.com
+        </span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
